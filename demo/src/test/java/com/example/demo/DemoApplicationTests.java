e1cbe5eeff159f7ca38c50d42116154b4c8c1e19
Christian Trutz <info@trutz-software-consulting.de>
2020-11-06 09:34:14 +0100

    Kapitel 03, Video 03

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +17,18 @@ class DemoApplicationTests {
 
 	@Test
 	void testInsert1000Docs() {
+		personRepository.deleteAll();
+		Assertions.assertEquals(0, personRepository.count());
+		
+		List<Person> personen = Stream.iterate(1, i -> i+1).limit(1000) //
+			.map(i -> {
+				Person person = new Person();
+				person.setName("Person "+ i);
+				return person;
+			}) //
+			.collect(Collectors.toList());
+		personRepository.insert(personen);
+		Assertions.assertEquals(1000, personRepository.count());
 	}
 
 }
