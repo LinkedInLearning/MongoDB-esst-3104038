027b56a7a9e46b0bde7507a8d99ed5abd1285751
Christian Trutz <info@trutz-software-consulting.de>
2020-11-11 13:06:07 +0100

    Kapitel 03, Sollution

--- demo/src/main/java/com/example/demo/MainView.java
+++ demo/src/main/java/com/example/demo/MainView.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -13,7 +16,18 @@ public class MainView extends VerticalLayout {
 	private static final long serialVersionUID = 7160713657316969767L;
 
 	public MainView(@Autowired PersonRepository personRepository) {
-		add(new Text("Personen"));
+		long anzahl = personRepository.count();
+		String titel = "Personen (" + anzahl + ")";
+		add(new Text(titel));
+
+		Button insertButton = new Button(new Icon(VaadinIcon.PLUS));
+		insertButton.addClickListener(event -> {
+			Person person = new Person();
+			person.setName("Christian Trutz");
+			personRepository.insert(person);
+		});
+		add(insertButton);
+
 		Grid<Person> grid = new Grid<>(Person.class);
 		grid.setItems(personRepository.findAll());
 		grid.setColumns("id", "name");
